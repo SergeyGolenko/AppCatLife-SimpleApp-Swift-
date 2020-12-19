114f753cf7c415140417025945d3f1eef33b0a30
SergeyGolenko <1989sergiotay@gmail.com>
2020-12-19 14:18:33 +0200

    приложение завершённо

--- AppCatLife(Simple)/ViewController.swift
+++ AppCatLife(Simple)/ViewController.swift
@@ -8,6 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var addOldForCatTextField: UITextField!
+    
+    
+    @IBOutlet weak var oldCat: UILabel!
+    
+    @IBAction func getAgeButton(_ sender: UIButton) {
+        
+        if let age = addOldForCatTextField.text{
+            if let age2 = Int(age){
+                
+                oldCat.text = "Your cat is \(age2 * 7) in cat years"
+                
+            }
+        }
+        
+    }
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
